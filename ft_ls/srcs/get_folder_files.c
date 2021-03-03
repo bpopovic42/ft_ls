012bcab3208c914f3e729c024694aba8c32aefce
boby <bpopovi42@gmail.com>
2021-03-03 17:21:57 +0100

    Function refactor completed

--- ft_ls/srcs/get_folder_files.c
+++ ft_ls/srcs/get_folder_files.c
@@ -1,23 +1,30 @@
 #include "ft_ls.h"
 
+int add_file_to_node(t_file *file, t_node **node)
+{
+	if (!(*node = ft_node_new(NULL, 0)))
+		return (EXIT_FAILURE);
+	(*node)->data      = file;
+	(*node)->data_size = file->struct_size;
+	return (EXIT_SUCCESS);
+}
+
 int add_new_file_to_folder(t_file *folder, struct dirent *entry)
 {
 	t_node        *new_node;
 	t_file        *new_file;
 
-	new_node   = NULL;
+	new_node = NULL;
 	new_file   = NULL;
 	if (entry->d_name[0] == '.' && g_flags[0] != 'a')
 		return (EXIT_SUCCESS);
 	if (create_new_file(&new_file, entry->d_name, folder->path) != EXIT_SUCCESS)
 		return (EXIT_FAILURE);
-	if (!(new_node = ft_node_new(NULL, 0)))
+	if (add_file_to_node(new_file, &new_node) != EXIT_SUCCESS)
 	{
 		del_file(new_file, new_file->struct_size);
 		return (EXIT_FAILURE);
 	}
-	new_node->data      = new_file;
-	new_node->data_size = new_file->struct_size;
 	ft_lstadd(folder->files, new_node);
 	return (EXIT_SUCCESS);
 }

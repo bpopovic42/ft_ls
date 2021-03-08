040f546cf58584a6cd585c61aced60724da2b6b2
boby <bpopovi42@gmail.com>
2021-03-08 20:35:54 +0100

    Fix double slash if parent folder is '/'

--- ft_ls/srcs/get_folder_files.c
+++ ft_ls/srcs/get_folder_files.c
@@ -13,12 +13,15 @@ int add_new_file_to_folder(t_file *folder, struct dirent *direntry)
 {
 	t_node        *new_node;
 	t_file        *new_file;
+	char            *parent_path;
 
 	new_node = NULL;
 	new_file   = NULL;
+	parent_path = ft_strequ(folder->path, "/") ? "" : folder->path;
 	if (direntry->d_name[0] == '.' && g_flags[0] != 'a')
 		return (EXIT_SUCCESS);
-	if (create_new_file(&new_file, direntry->d_name, folder->path) != EXIT_SUCCESS)
+	if (create_new_file(&new_file, direntry->d_name, parent_path) !=
+	EXIT_SUCCESS)
 		return (EXIT_FAILURE);
 	if (add_file_to_node(new_file, &new_node) != EXIT_SUCCESS)
 	{

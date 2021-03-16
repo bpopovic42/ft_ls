e77019e5b5be0106bb591e1f023f11d2a05e6b6c
boby <bpopovi42@gmail.com>
2021-03-17 00:02:35 +0100

    Implementing tests

--- bin/dev/bash.sh
+++ bin/dev/bash.sh
@@ -2,9 +2,11 @@
 
 PWD=$(pwd)
 PROJECT_NAME=$(cat $PWD/.project_name)
+TEST_DIR="tests"
 
 docker run --rm -ti --user `id -u` --name ${PROJECT_NAME}-bash \
-	-v ${PWD}/${PROJECT_NAME}:/workspace \
+	-v ${PWD}/${PROJECT_NAME}:/workspace/${PROJECT_NAME} \
+	-v ${PWD}/${TEST_DIR}:/workspace/${TEST_DIR} \
 	--net host \
 	-w /workspace \
 	${PROJECT_NAME}-dev /bin/bash

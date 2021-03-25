a511a39d74eee0f440a8b8f1ab908c7e7188155b
boby <bpopovi42@gmail.com>
2021-03-25 02:10:30 +0100

    Added tests

--- tests/test_suites/test_l_flag.py
+++ tests/test_suites/test_l_flag.py
@@ -10,7 +10,40 @@ def test_l_flag_1():
 
 def test_l_flag_2():
     sandbox = Sandbox("test_l_flag_2", "-l")
-    sandbox.cmd("touch file && xattr -w theAnswerIs42 whatever_you_want file")
+    sandbox.cmd("touch .a")
+    sandbox.cmd("dd count=14450 if=/dev/zero of=.a  >/dev/null 2>&1")
+    sandbox.cmd("ln -s .a b")
+    return sandbox.run()
+
+
+def test_l_flag_3():
+    sandbox = Sandbox("test_l_flag_3", "-la dir")
+    sandbox.cmd("mkdir -p dir/.hdir")
+    sandbox.cmd("touch dir/.hdir/file")
+    return sandbox.run()
+
+
+def test_l_flag_4():
+    sandbox = Sandbox("test_l_flag_4", "-l dir")
+    sandbox.cmd("mkdir -p dir")
+    sandbox.cmd("touch dir/.file touch dir/visible")
+    sandbox.cmd("ln dir/.file sym0")
+    sandbox.cmd("ln dir/.file sym1")
+    sandbox.cmd("ln dir/.file sym2")
+    sandbox.cmd("ln dir/.file sym3")
+    sandbox.cmd("ln dir/.file sym4")
+    sandbox.cmd("ln dir/.file sym5")
+    sandbox.cmd("ln dir/.file sym6")
+    sandbox.cmd("ln dir/.file sym7")
+    sandbox.cmd("ln dir/.file sym8")
+    sandbox.cmd("ln dir/.file sym9")
+    sandbox.cmd("ln dir/.file sym10")
+    return sandbox.run()
+
+
+def test_l_flag_5():
+    sandbox = Sandbox("test_l_flag_5", "-lt")
+    sandbox.cmd("touch -t 999912312359 future")
     return sandbox.run()
 
 
@@ -23,4 +56,7 @@ def test_l_flag_padding():
 def run_test_l_flag():
     test_l_flag_1()
     test_l_flag_2()
+    test_l_flag_3()
+    test_l_flag_4()
+    test_l_flag_5()
     test_l_flag_padding()

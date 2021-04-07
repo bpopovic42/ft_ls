d33fa80dcc094bb964c761805fdd5542e93de2d4
boby <bpopovi42@gmail.com>
2021-04-07 10:52:04 +0200

    Added symlink tests

---
+++ tests/test_suites/test_symlink.py
@@ -0,0 +1,37 @@
+from testing_utils.sandbox import Sandbox
+
+
+def test_symlink_1():
+    sandbox = Sandbox("test_symlink_1", "symdir")
+    sandbox.cmd("mkdir mydir && ln -s mydir symdir && touch mydir/file{1..5}")
+    return sandbox.run()
+
+
+def test_symlink_2():
+    sandbox = Sandbox("test_symlink_2", "b")
+    sandbox.cmd("touch a")
+    sandbox.cmd("ln -s a b")
+    return sandbox.run()
+
+
+def test_symlink_3():
+    sandbox = Sandbox("test_symlink_3", "b")
+    sandbox.cmd("mkdir a")
+    sandbox.cmd("ln -s a b")
+    sandbox.cmd("rm -rf a")
+    return sandbox.run()
+
+
+def test_symlink_4():
+    sandbox = Sandbox("test_symlink_4", "-l b")
+    sandbox.cmd("mkdir a")
+    sandbox.cmd("ln -s a b")
+    sandbox.cmd("rm -rf a")
+    return sandbox.run()
+
+
+def run_test_symlink():
+    test_symlink_1()
+    test_symlink_2()
+    test_symlink_3()
+    test_symlink_4()

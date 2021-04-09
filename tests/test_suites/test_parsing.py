33aa8f44c9e81c25d317f8c613fc6ed63cf1c16f
boby <bpopovi42@gmail.com>
2021-04-09 10:35:47 +0200

    Added yet another failing parsing test

--- tests/test_suites/test_parsing.py
+++ tests/test_suites/test_parsing.py
@@ -15,8 +15,16 @@ def test_parsing_ending_multi_slash():
     return sandbox.run()
 
 
+def test_parsing_repeated_slashes():
+    sandbox = Sandbox("test_parsing_repeated_slashes", "-lR dir_01////dir_02///dir_03///")
+    sandbox.cmd("mkdir dir_01 && mkdir dir_01/dir_02 && mkdir dir_01/dir_02/dir_03 && mkdir dir_01/dir_02/dir_03/dir_04")
+    sandbox.cmd("touch dir_01/dir_02/dir_03/file_03 dir_01/dir_02/dir_03/file_04 dir_01/dir_02/dir_03/dir_04/file_05")
+    return sandbox.run()
+
+
 def get_tests_parsing():
     return [
         test_parsing_ending_slash,
-        test_parsing_ending_multi_slash
+        test_parsing_ending_multi_slash,
+        test_parsing_repeated_slashes
     ]

5904f2a3ff49405fc5bb67548a8d11450ebc9dab
boby <bpopovi42@gmail.com>
2021-04-10 10:15:23 +0200

    Restructured tester

--- testing/tester/testing_utils/globals.py
+++ testing/tester/testing_utils/globals.py
@@ -2,7 +2,7 @@ import os
 import subprocess as sb
 
 LS_PATH = sb.check_output(["which", "ls"]).decode("utf-8").replace("\n", "")
-FT_LS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../ft_ls/ft_ls"))
+FT_LS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../ft_ls/ft_ls"))
 SANDBOXES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../sandbox"))
 COLOR_GREEN = '\033[92m'
 COLOR_RED = '\033[91m'

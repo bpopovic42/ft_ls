a206166e6496875f63608f58119c04c7d59bf0fa
boby <bpopovi42@gmail.com>
2021-04-22 15:43:07 +0200

    Command-line tester wasn't updated to new printing mechanism

--- testing/tester/testing_utils/test_from_cli.py
+++ testing/tester/testing_utils/test_from_cli.py
@@ -1,8 +1,10 @@
 import os
 
+from testing_utils import sandbox
 from testing_utils.sandbox import Sandbox
 
 
 def test_from_cli(arguments):
-    sandbox = Sandbox("cli_test", arguments, provided_working_dir=os.getcwd(), print_results=True)
-    sandbox.run()
+    sandbox.Sandbox.set_printing(True)
+    cli_sandbox = Sandbox("cli_test", arguments, provided_working_dir=os.getcwd())
+    cli_sandbox.run()

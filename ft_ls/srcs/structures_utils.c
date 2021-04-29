523f5e8d5a92496b6e5e37ae18a91bbfb1929cc1
boby <bpopovi42@gmail.com>
2021-04-29 20:54:32 +0200

    Program name wasn't set if prior memory allocations failed

--- ft_ls/srcs/structures_utils.c
+++ ft_ls/srcs/structures_utils.c
@@ -3,13 +3,13 @@
 int init_store(t_store *store, char *program_name)
 {
 	ft_bzero(store, sizeof(t_store));
+	store->program_name = program_name;
 	if (!(store->folders_queue = ft_lstnew()))
 		return (handle_error(store, FT_LS_FATAL_ERROR));
 	if (!(store->invalid_folders = ft_lstnew()))
 		return (handle_error(store, FT_LS_FATAL_ERROR));
 	if (create_new_file(&store->cli_file_arguments_folder, "", "") != EXIT_SUCCESS)
 		return (handle_error(store, FT_LS_FATAL_ERROR));
-	store->program_name = program_name;
 	return (EXIT_SUCCESS);
 }
 
